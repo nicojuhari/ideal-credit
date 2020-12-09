b95c394b248dbba73077e356d760f1f982692672
Nicolae Cojuhari <nicojuhari@gmail.com>
2020-12-09 15:58:48 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  plugins: [
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "UA-80593294-1",
+      },
+    },
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-offline",
+    "gatsby-plugin-manifest",
+  ],
+};
