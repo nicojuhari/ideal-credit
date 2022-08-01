ffa497d720c1f908d0dbb7785d4191fb063c09df
Nicolae Cojuhari <nicojuhari@gmail.com>
2022-08-01 11:00:44 +0200

    adds google0site-verification

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,13 +13,13 @@ export default defineNuxtConfig({
                 ],
                 meta: [
                     { name: 'description', content: 'Credite nebancare pentru consum și afaceri, Chișinău, Republica Moldova' },
-                    { name: 'keywords', content: 'Credite rapide, credite nebancare, credite Chișinău, credite md'}
+                    { name: 'keywords', content: 'Credite rapide, credite nebancare, credite Chișinău, credite md'},
+                    { name: 'google-site-verification', content: "xSvHXCUVs_GmLMJ17te-PeWngkTtzV-pDoMRviDCcV0"}
                 ],
             },
     },
     modules: ['@nuxtjs/tailwindcss'],
     css: ['@/assets/css/main.css'],
-     
     nitro: {
         prerender: {
         routes: ['/', '/despre-noi', '/contacte']
