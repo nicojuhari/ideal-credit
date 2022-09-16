9489bf02cd24b5ee102cb22c8301e273b66c800f
Nicolae Cojuhari <nicojuhari@gmail.com>
2022-09-16 13:07:32 +0200

    modified gtag to defer

--- plugins/gtag.client.js
+++ plugins/gtag.client.js
@@ -3,11 +3,12 @@ export default defineNuxtPlugin(() => {
     if(process.dev) return
 
     let script = document.createElement('script')
-    script.setAttribute('async', '')
+    script.setAttribute('defer', '')
     script.setAttribute("src", "https://www.googletagmanager.com/gtag/js?id=G-YXDZGPPXPH");
     document.head.appendChild(script)
 
     let script2 = document.createElement('script')
+    script2.setAttribute("defer", "");
     script2.innerHTML = `
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
