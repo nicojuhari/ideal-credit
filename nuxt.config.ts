42f35e3271e2c86437f7368d425b2ef80d55a8fc
Nicolae Cojuhari <nicojuhari@gmail.com>
2023-07-07 11:44:34 +0300

    adds partytown

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,9 +16,17 @@ export default defineNuxtConfig({
                     { name: 'keywords', content: 'Credite rapide, credite nebancare, împrumut, credite nevoi personale, credite Chișinău, credite Moldova, credite pentru afaceri' },
                     { name: 'google-site-verification', content: "xSvHXCUVs_GmLMJ17te-PeWngkTtzV-pDoMRviDCcV0"}
                 ],
+                script: [
+                    // Insert your Google Tag Manager Script here
+                    { src: 'https://www.googletagmanager.com/gtag/js?id=G-YXDZGPPXPH', async: true, type: 'text/partytown' },
+                    { children: "window.dataLayer = window.dataLayer || []; function gtag(){ dataLayer.push(arguments); } gtag('js', new Date());gtag('config', 'G-YXDZGPPXPH');" },
+                ],
             },
     },
-    modules: ['@nuxtjs/tailwindcss','@formkit/nuxt', 'nuxt-simple-sitemap', 'nuxt-icon'],
+    partytown: {
+        forward: ['dataLayer.push'],
+    },
+    modules: ['@nuxtjs/tailwindcss','@formkit/nuxt', 'nuxt-simple-sitemap', 'nuxt-icon', '@nuxtjs/partytown'],
     extends: [
         'nuxt-seo-kit'
     ],
--- plugins/gtag.client.js
+++ plugins/gtag.client.js
@@ -2,19 +2,19 @@ export default defineNuxtPlugin(() => {
     
     if(process.dev) return
 
-    let script = document.createElement('script')
-    script.setAttribute('defer', '')
-    script.setAttribute("src", "https://www.googletagmanager.com/gtag/js?id=G-YXDZGPPXPH");
-    document.head.appendChild(script)
+    // let script = document.createElement('script')
+    // script.setAttribute('defer', '')
+    // script.setAttribute("src", "https://www.googletagmanager.com/gtag/js?id=G-YXDZGPPXPH");
+    // document.head.appendChild(script)
 
-    let script2 = document.createElement('script')
-    script2.setAttribute("defer", "");
-    script2.innerHTML = `
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-YXDZGPPXPH');`;
+    // let script2 = document.createElement('script')
+    // script2.setAttribute("defer", "");
+    // script2.innerHTML = `
+    //     window.dataLayer = window.dataLayer || [];
+    //     function gtag(){dataLayer.push(arguments);}
+    //     gtag('js', new Date());
+    //     gtag('config', 'G-YXDZGPPXPH');`;
     
-    document.head.appendChild(script2);
+    // document.head.appendChild(script2);
 
 });
