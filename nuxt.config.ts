bff27d32b40aa019cb393c8326392eed3d32491e
Nicolae Cojuhari <nicojuhari@gmail.com>
2023-07-25 18:25:57 +0300

    upgrade packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,25 +1,22 @@
 export default defineNuxtConfig({
     app: {
         head: { 
-                htmlAttrs: {
-                    lang: 'ro'
-                },
-                title: 'Credite rapide pentru nevoi personale și afaceri - Ideal Credit',
-                link: [
-                    { rel: 'icon', type:'image/x-icon', href: '/favicon.ico' },
-                    { rel: 'shortcut icon', type:'image/png', sizes:"32x32", href:"/favicon-32x32.png" },
-                    { rel: 'shortcut icon', type:'image/png', sizes:"16x16", href:"/favicon-16x16.png" },
-                    { rel: 'apple-touch-icon', sizes:"180x180", href:"/apple-touch-icon.png" }
-                ],
-                meta: [
-                    { name: 'description', content: 'Credite rapide pentru nevoi personale și afaceri în Chișinău, Republica Moldova cu dobânzi avantajoase și fără comisioane ascunse' },
-                    { name: 'keywords', content: 'Credite rapide, credite nebancare, împrumut, credite nevoi personale, credite Chișinău, credite Moldova, credite pentru afaceri' },
-                    { name: 'google-site-verification', content: "xSvHXCUVs_GmLMJ17te-PeWngkTtzV-pDoMRviDCcV0"}
-                ],
+            htmlAttrs: {
+                lang: 'ro'
             },
-    },
-    partytown: {
-        forward: ['dataLayer.push'],
+            title: 'Credite rapide pentru nevoi personale și afaceri - Ideal Credit',
+            link: [
+                { rel: 'icon', type:'image/x-icon', href: '/favicon.ico' },
+                { rel: 'shortcut icon', type:'image/png', sizes:"32x32", href:"/favicon-32x32.png" },
+                { rel: 'shortcut icon', type:'image/png', sizes:"16x16", href:"/favicon-16x16.png" },
+                { rel: 'apple-touch-icon', sizes:"180x180", href:"/apple-touch-icon.png" }
+            ],
+            meta: [
+                { name: 'description', content: 'Credite rapide pentru nevoi personale și afaceri în Chișinău, Republica Moldova cu dobânzi avantajoase și fără comisioane ascunse' },
+                { name: 'keywords', content: 'Credite rapide, credite nebancare, împrumut, credite nevoi personale, credite Chișinău, credite Moldova, credite pentru afaceri' },
+                { name: 'google-site-verification', content: "xSvHXCUVs_GmLMJ17te-PeWngkTtzV-pDoMRviDCcV0"}
+            ],
+        },
     },
     modules: ['@nuxtjs/tailwindcss','@formkit/nuxt', 'nuxt-simple-sitemap', 'nuxt-icon'],
     extends: [
@@ -32,7 +29,7 @@ export default defineNuxtConfig({
             siteUrl: process.env.NUXT_PUBLIC_SITE_URL || 'https://idealcredit.md',
             siteName: 'Ideal Credit',
             siteDescription: 'Credite rapide pentru nevoi personale și afaceri în Chișinău, Republica Moldova cu dobânzi avantajoase și fără comisioane ascunse',
-            language: 'ro', // prefer more explicit language codes like `en-AU` over `en`
+            language: 'ro-RO', // prefer more explicit language codes like `en-AU` over `en`
         }
     },
     sitemap: {
