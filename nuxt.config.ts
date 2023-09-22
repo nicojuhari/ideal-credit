a01358edbaf8f650728e6523ed8dda616139b4ad
Nicolae Cojuhari <nicojuhari@gmail.com>
2023-09-22 13:08:17 +0200

    adds splitbee analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,9 @@ export default defineNuxtConfig({
                 lang: 'ro'
             },
             title: 'Credite rapide pentru nevoi personale și afaceri',
+            script: [
+                { src: 'https://cdn.splitbee.io/sb.js', async: true}
+            ],
             link: [
                 { rel: 'icon', type:'image/x-icon', href: '/favicon.ico' },
                 { rel: 'shortcut icon', type:'image/png', sizes:"32x32", href:"/favicon-32x32.png" },
