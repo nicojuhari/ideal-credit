5aec6c096737f7f067603bbec674297ca538f22b
Nicolae Cojuhari <nicojuhari@gmail.com>
2024-09-12 16:07:43 +0200

    replaced axios with $fetch

--- server/api/cerere-online.post.ts
+++ server/api/cerere-online.post.ts
@@ -9,24 +9,23 @@ export default defineEventHandler(async (event) => {
     try {
         if(apiURL && body) {
 
-            const headers = {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-                'Accept-Language': 'en-US,en;q=0.5',
-                'Referer': apiURL,
-                'DNT': '1',
-                'Connection': 'keep-alive',
-                'Upgrade-Insecure-Requests': '1',
-                'Content-Type': 'application/json',
-            }
+            // const headers = {
+            //     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+            //     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            //     'Accept-Language': 'en-US,en;q=0.5',
+            //     'Referer': apiURL,
+            //     'DNT': '1',
+            //     'Connection': 'keep-alive',
+            //     'Upgrade-Insecure-Requests': '1',
+            //     'Content-Type': 'application/json',
+            // }
             // 
-            let response = await axios.post( apiURL + '/cerere_online?secret_key=' + config.secret_key, 
-                { 
-                    body, 
-                    headers 
+            let response = await $fetch( apiURL + '/cerere_online?secret_key=' + config.secret_key, {   
+                    method: 'POST',
+                    body,
                 })
 
-            console.log(response.data)
+            console.log(response)
             return { success: 'Cererea a fost transmisă cu success!'}
         }
         console.log(body)
