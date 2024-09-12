9f0321e66eff89b3897b91098a4bf2a74f2721e7
Nicolae Cojuhari <nicojuhari@gmail.com>
2024-09-12 15:41:07 +0200

    adds headers to request

--- server/api/cerere-online.post.ts
+++ server/api/cerere-online.post.ts
@@ -8,7 +8,18 @@ export default defineEventHandler(async (event) => {
     console.log(apiURL + '/cerere_online?secret_key=' + config.secret_key, 'server/api')
     try {
         if(apiURL && body) {
-            let response = await axios.post(apiURL + '/cerere_online?secret_key=' + config.secret_key, body)
+
+            const headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+                'Accept-Language': 'en-US,en;q=0.5',
+                'Referer': apiURL,
+                'DNT': '1',
+                'Connection': 'keep-alive',
+                'Upgrade-Insecure-Requests': '1'
+            }
+            // 
+            let response = await axios.post(apiURL + '/cerere_online?secret_key=' + config.secret_key, body, { headers })
 
             console.log(response.data)
             return { success: 'Cererea a fost transmisă cu success!'}
