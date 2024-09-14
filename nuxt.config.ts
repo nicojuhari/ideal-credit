43f5877cf26ea1c8578e63707ab7cd11a05752f4
Nicolae Cojuhari <nicojuhari@gmail.com>
2024-09-14 16:02:29 +0200

    convert icons to svg

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,34 +1,37 @@
 export default defineNuxtConfig({
-  app: {
-      head: { 
-          htmlAttrs: {
-              lang: 'ro'
-          },
-          title: 'Credite rapide - pentru nevoi personale și afaceri',
-          script: [
-              { src: 'https://cdn.splitbee.io/sb.js', async: true}
-          ],
-          link: [
-              { rel: 'icon', type:'image/x-icon', href: '/favicon.ico' },
-              { rel: 'shortcut icon', type:'image/png', sizes:"32x32", href:"/favicon-32x32.png" },
-              { rel: 'shortcut icon', type:'image/png', sizes:"16x16", href:"/favicon-16x16.png" },
-              { rel: 'apple-touch-icon', sizes:"180x180", href:"/apple-touch-icon.png" }
-          ],
-          meta: [
-              { name: 'description', content: 'Oferim credite rapide nebancare, pentru nevoi personale și afaceri în Chișinău, Republica Moldova. Dobânzi mici și fără comisioane ascunse. Aplică online!' },
-              { name: 'keywords', content: 'credit rapid, credit online, împrumut, credit nevoi personale, credit Chișinău Moldova, credit online md, credit avantajos' },
-              { name: 'google-site-verification', content: "xSvHXCUVs_GmLMJ17te-PeWngkTtzV-pDoMRviDCcV0"}
-          ],
-      },
-  },
-
-  modules: ['@nuxtjs/tailwindcss', '@formkit/nuxt', '@nuxtjs/seo', [ "@storyblok/nuxt", { accessToken: 'HkdYYsU6W0SQKNL9nL1seQtt' }], '@nuxt/icon'],
-  css: ['@/assets/css/main.css'],
-
-  tailwindcss: {
-      cssPath: '~/assets/css/main.css',
-  },
-
+    app: {
+        head: { 
+            htmlAttrs: {
+                lang: 'ro'
+            },
+            title: 'Credite rapide - pentru nevoi personale și afaceri',
+            script: [
+                { src: 'https://cdn.splitbee.io/sb.js', async: true}
+            ],
+            link: [
+                { rel: 'icon', type:'image/x-icon', href: '/favicon.ico' },
+                { rel: 'shortcut icon', type:'image/png', sizes:"32x32", href:"/favicon-32x32.png" },
+                { rel: 'shortcut icon', type:'image/png', sizes:"16x16", href:"/favicon-16x16.png" },
+                { rel: 'apple-touch-icon', sizes:"180x180", href:"/apple-touch-icon.png" }
+            ],
+            meta: [
+                { name: 'description', content: 'Oferim credite rapide nebancare, pentru nevoi personale și afaceri în Chișinău, Republica Moldova. Dobânzi mici și fără comisioane ascunse. Aplică online!' },
+                { name: 'keywords', content: 'credit rapid, credit online, împrumut, credit nevoi personale, credit Chișinău Moldova, credit online md, credit avantajos' },
+                { name: 'google-site-verification', content: "xSvHXCUVs_GmLMJ17te-PeWngkTtzV-pDoMRviDCcV0"}
+            ],
+        },
+    },
+    modules: ['@nuxtjs/tailwindcss', '@formkit/nuxt', '@nuxtjs/seo', [ "@storyblok/nuxt", { accessToken: 'HkdYYsU6W0SQKNL9nL1seQtt' }], '@nuxt/icon'],
+    css: ['@/assets/css/main.css'],
+    tailwindcss: {
+        cssPath: '~/assets/css/main.css',
+    },
+    icon: {
+        serverBundle: {
+            collections: ['ph']
+        },
+        mode: 'svg',
+    },
     runtimeConfig: {
         siteUrl: 'https://idealcredit.md',
         icm_api_url: process.env.NUXT_ICM_API_URL,
@@ -43,21 +46,21 @@ export default defineNuxtConfig({
         }
     },
 
-  site: {
-      url: process.env.NUXT_BASE_URL || 'https://idealcredit.md',
-      name: 'Ideal Credit',
-  },
+    site: {
+        url: process.env.NUXT_BASE_URL || 'https://idealcredit.md',
+        name: 'Ideal Credit',
+    },
 
-  sitemap: {
-  // exclude all URLs that start with /secret
-      exclude: ['/autoritatea-de-supraveghere'],
-      sources: [
-          '/api/__sitemap__/urls',
-      ]
-  },
-  devServer: {
-    port: 3001
-  },
+    sitemap: {
+    // exclude all URLs that start with /secret
+        exclude: ['/autoritatea-de-supraveghere'],
+        sources: [
+            '/api/__sitemap__/urls',
+        ]
+    },
+    devServer: {
+        port: 3001
+    },
 
   compatibilityDate: '2024-09-04'
 })
