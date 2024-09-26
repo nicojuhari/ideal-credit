70e4350496c3e8d3f752a69e13f6db391cca27d6
Nicolae Cojuhari <nicojuhari@gmail.com>
2024-09-26 14:05:51 +0200

    fix: copy

--- components/ShortAboutUs.vue
+++ components/ShortAboutUs.vue
@@ -7,7 +7,7 @@
         <div class="text-lg">
             <p>Pe piața financiară din Moldova, Ideal Credit SRL se remarcă prin angajamentul său de a oferi soluții financiare accesibile și transparente.</p>
             <p>Am ajutat sute de clienți să-și atingă obiectivele personale și profesionale cu milioane de lei de credite acordate.</p>
-            <p>Oferim credite nebancare, rapid și avantajos, astfel încât clienții noștri să aibă un timp plăcut și fără surprize neplăcute.</p>
+            <p>Oferim credite nebancare, rapid și avantajos, asigurând astfel o experiență plăcută și fără surprize.</p>
             <p>Ne considerăm un partener de încredere în realizarea visurilor financiare ale fiecărei persoane, datorită dedicației noastre față de satisfacția clienților și flexibilității noastre în procesul de creditare.</p>
         </div>
     </div>
