9dbf3ea26044e7597663c90472b0dfd75bcced29
Nicolae Cojuhari <nicojuhari@gmail.com>
2024-09-27 17:09:58 +0200

    fix:sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -54,7 +54,7 @@ export default defineNuxtConfig({
 
     sitemap: {
     // exclude all URLs that start with /secret
-        exclude: ['/autoritatea-de-supraveghere', '/cookies', '/privacy', '/terms'],
+        exclude: ['/autoritatea-de-supraveghere'],
         sources: [
             '/api/__sitemap__/urls',
         ]
