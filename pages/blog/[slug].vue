1717f4dbaac8eb4d99297bb6f44ee8a25f766692
Nicolae Cojuhari <nicojuhari@gmail.com>
2025-04-04 10:04:25 +0200

    fix colors on blog

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -41,11 +41,11 @@ useSchemaOrg([
 
 <template>
     <section class="py-5 md:py-10 blog-page">
-        <div class="container sm-container card light" v-if="story">
+        <div class="container sm-container card" v-if="story">
             <h1 class="text-3xl mb-6 font-bold">{{ story?.name }}</h1>
             <div>
                 <img :src="story.content?.image?.filename" :alt="story.content.image.alt | story.name"
-                    class="w-full bg-slate-100 object-center object-cover border-0 rounded aspect-video" />
+                    class="w-full object-center object-cover border-0 rounded aspect-video" />
             </div>
             <div class="mt-6">
                 <div class="richtext text-lg" v-html="htmlText">
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -28,20 +28,17 @@ useSchemaOrg([
 <template>
     <section class="px-4 py-10 md:py-20">
         <div class="container">
-            <h1 class="subtitle text-center my-10">Blog</h1>
+            <h1 class="title text-center my-10">Blog financiar</h1>
             <div class="gap-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
-                <NuxtLink v-for="{ content, full_slug, published_at } in stories" :to="`/${full_slug}`" :key="content.id" :title="content.title"
-                    class="group cursor-pointer flex-shrink-0 bg-white text-orange-black rounded-xl overflow-hidden shadow-xl">
+                <NuxtLink v-for="{ content, full_slug } in stories" :to="`/${full_slug}`" :key="content.id" :title="content.title"
+                    class="group cursor-pointer flex-shrink-0 bg-black-100 rounded-xl overflow-hidden shadow-xl">
                     <div class="rounded-t-xl overflow-hidden">
                             <img :src="content.image.filename" :alt="content.title" 
                                 class="w-full object-center object-cover border-0 group-hover:scale-125 duration-300" />
                             </div>
                         <div class="p-6">
-                            <div class="font-bold text-lg line-clamp-2  duration-300 ">
+                            <div class="font-bold text-lg line-clamp-2 duration-300 ">
                                 {{ content.title }}
-                            </div>
-                            <div class="flex pt-6 text-sm">
-                                {{ new Date(published_at).toLocaleDateString('ro-RO') }}
                             </div>
                         </div>
                 </NuxtLink>
