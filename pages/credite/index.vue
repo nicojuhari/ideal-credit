f92f1165cd7ebf73791b752fbd8dbd24292ab3af
Nicolae Cojuhari <nicojuhari@gmail.com>
2025-09-29 20:24:04 +0200

    new title for credite page

--- pages/credite/index.vue
+++ pages/credite/index.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
     useHead({
-    title: 'Credite de la Ideal Credit',
+    title: 'Credite pentru persoane fizice și juridice | Ideal Credit',
     titleTemplate: '%pageTitle',
     meta: [
         { name: 'description', content: 'Credite pentru persoane fizice și juridice în Moldova. Soluții financiare urgente pentru nevoi personale sau investiții în afaceri - simplu și sigur!' },
@@ -11,7 +11,7 @@
 <template>
         <section class="container">
             <h1 class="font-semibold text-center text-6xl md:text-8xl">Credite</h1>
-            <h2 class="text-center mt-4 px-4 md:px-0 text-gray-400 md:text-xl md:max-w-xl mx-auto text-lg font-light">Finanțare simplă pentru nevoi personale și afaceri.</h2>
+            <h2 class="text-center mt-4 px-4 md:px-0 text-gray-400 md:text-xl md:max-w-xl mx-auto text-lg font-light">Finanțare simplă pentru persoane fizice și juridice.</h2>
         </section>
         <section class="container">
             <ServiciiList />
